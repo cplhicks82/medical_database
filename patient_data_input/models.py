e652ca34d84956a78782f3ab1e79eeb87bf7bba9
Ben Vaughn <bevon@protonmail.com>
2020-12-22 11:32:38 -0700

    Started basic html and created class for patient input.

--- patient_data_input/models.py
+++ patient_data_input/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class PatientInput(models.Model):
+	"""Defining the fields for new patient input."""
+	PatientKey = models.CharField(max_length=50)
+	DoctorKey = models.CharField(max_length=50)
+	FirstName = models.CharField(max_length=50)
+	MiddleName = models.CharField(max_length=50)
+	LastName = models.CharField(max_length=50)
+	PhoneNumber = models.CharField(max_length=50)
--- patient_data_input/views.py
+++ patient_data_input/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from models import PatientInput
+from django.db import connection
 
-# Create your views here.
+def save_patient(request):
+	if request.method == 'POST':
+		if request.POST.get('PatientKey') and request.POST.get('DoctorKey')
+		and request.POST.get('FirstName') and request.POST.get('MiddleName')
+		and request.POST.get('LastName') and request.POST.get('PhoneNumber'):
+			patient_save = PatientInput()
+			patient_save.PatientKey = request.POST.get('PatientKey')
+			patient_save.DoctorKey = request.POST.get('DoctorKey')
+			patient_save.FirstName = request.POST.get('FirstName')
+			patient_save.MiddleName = request.POST.get('MiddleName')
+			patient_save.LastName = request.POST.get('LastName')
+			patient_save.PhoneNumber = request.POST.get('PhoneNumber')
+			cursor = connection.cursor()
