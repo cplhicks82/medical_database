f46eea309b69cbbe08034f53274465e960006c2d
Ben Vaughn <bevon@protonmail.com>
2020-12-23 21:56:41 -0700

    Finished project; created views, index, and the stored procedure call.

--- patient_data_input/views.py
+++ patient_data_input/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from models import PatientInput
 from django.db import connection
+from django.contrib import messages
+from .models import PatientInput
 
 def save_patient(request):
 	if request.method == 'POST':
-		if request.POST.get('PatientKey') and request.POST.get('DoctorKey')
-		and request.POST.get('FirstName') and request.POST.get('MiddleName')
+		if request.POST.get('PatientKey') and request.POST.get('DoctorKey')\
+		and request.POST.get('FirstName') and request.POST.get('MiddleName')\
 		and request.POST.get('LastName') and request.POST.get('PhoneNumber'):
 			patient_save = PatientInput()
 			patient_save.PatientKey = request.POST.get('PatientKey')
@@ -14,4 +15,16 @@ def save_patient(request):
 			patient_save.MiddleName = request.POST.get('MiddleName')
 			patient_save.LastName = request.POST.get('LastName')
 			patient_save.PhoneNumber = request.POST.get('PhoneNumber')
+			
 			cursor = connection.cursor()
+
+			cursor.execute("EXECUTE usp_RecordPatient'"+patient_save.PatientKey+"','"+patient_save.DoctorKey+"',\
+				'"+patient_save.FirstName+"', '"+patient_save.MiddleName+"', '"+patient_save.LastName+"',\
+				'"+patient_save.PhoneNumber+"'")
+
+			messages.success(request, "The patient "+patient_save.FirstName+" "+patient_save.LastName+" was\
+			saved successfully.")
+
+			return render(request, 'patient_data_input/index.html')
+	else:
+			return render(request, 'patient_data_input/index.html')
